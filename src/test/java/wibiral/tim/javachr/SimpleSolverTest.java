917e2886d6e5248dff7f15bc691492a927d23a5c
Tim Wibiral <tim.wibiral@uni-ulm.de>
2020-08-21 18:45:00 +0200

    Added test for SimpleSolver

--- src/test/java/wibiral/tim/javachr/SimpleSolverTest.java
+++ src/test/java/wibiral/tim/javachr/SimpleSolverTest.java
@@ -1,8 +1,10 @@
 package wibiral.tim.javachr;
 
 import org.junit.Test;
+import wibiral.tim.javachr.constraints.Constraint;
 import wibiral.tim.javachr.constraints.ConstraintStore;
 import wibiral.tim.javachr.rules.Propagation;
+import wibiral.tim.javachr.rules.Simpagation;
 import wibiral.tim.javachr.rules.Simplification;
 
 import static org.junit.Assert.*;
@@ -11,7 +13,19 @@ public class SimpleSolverTest {
 
     @Test
     public void solve() {
+        RuleSet rules = getGCDRules();
+        ConstraintSolver solver = new SimpleSolver(rules);
+        ConstraintStore store;
 
+        store = new ConstraintStore(7, 28);
+        solver.solve(store);
+        assertEquals(1, store.size());
+        assertTrue(store.contains(7));
+
+        store = new ConstraintStore(9, 3);
+        solver.solve(store);
+        assertEquals(1, store.size());
+        assertTrue(store.contains(3));
     }
 
     @Test
@@ -28,9 +42,18 @@ public class SimpleSolverTest {
                 x -> (int) x[0].value() == 4 && (int) x[1].value() == 3 && (int) x[2].value() == 2 && (int) x[3].value() == 1
         );
 
-        System.out.println(asString(solver.findAssignment(store, rule1, new int[1], 0)));
-        System.out.println(asString(solver.findAssignment(store, rule2, new int[2], 0)));
-        System.out.println(asString(solver.findAssignment(store, rule3, new int[4], 0)));
+        int[] arr1 = solver.findAssignment(store, rule1, new int[1], 0);
+        assertEquals(0, arr1[0]);
+
+        int[] arr2 = solver.findAssignment(store, rule2, new int[2], 0);
+        assertEquals(3, arr2[0]);
+        assertEquals(3, arr2[1]);
+
+        int[] arr3 = solver.findAssignment(store, rule3, new int[4], 0);
+        assertEquals(3, arr3[0]);
+        assertEquals(2, arr3[1]);
+        assertEquals(1, arr3[2]);
+        assertEquals(0, arr3[3]);
     }
 
     String asString(int[] array){
@@ -62,19 +85,33 @@ public class SimpleSolverTest {
         assertTrue(solver.notAllEqual(array));
     }
 
-//    @Test
-//    public void possibleAssignments() {
-//        RuleSet rules = new RuleSet();
-//        rules.add(new Propagation(0));
-//        SimpleSolver solver = new SimpleSolver(rules);
-//        List<int[]> list = solver.possibleAssignments(4, 4);
-//        System.out.println("list.size() = " + list.size());
-//
-//        for(int[] array : list){
-//            for(int i : array)
-//                System.out.print(i + " ");
-//
-//            System.out.println();
-//        }
-//    }
+
+    RuleSet getGCDRules(){
+        RuleSet ruleSet = new RuleSet();
+        ruleSet.add(new Simpagation(1, 1).guard(
+                (h1, h2) ->
+                        // h1[0].value() instanceof Integer && h2[0].value() instanceof Integer &&
+                        // Not necessary if you can be sure that all Constraints are Integers.
+                        (int) h1[0].value() > 0 && (int) h1[0].value() <= (int) h2[0].value()
+        ).body(
+                (x1, x2, newConstraints) -> {
+                    int n = (int) x1[0].value();
+                    int m = (int) x2[0].value();
+                    newConstraints.add(new Constraint<>(m - n));
+                }
+        ));
+        ruleSet.add(new Simplification(1)
+                .guard(
+                        x ->
+                                // Not necessary if you can be sure that all Constraints are Integers.
+                                // x[0].value() instanceof Integer &&
+                                (int) x[0].value() == 0
+                )
+                .body(
+                        (x,y) -> {  }
+                )
+        );
+
+        return ruleSet;
+    }
 }
