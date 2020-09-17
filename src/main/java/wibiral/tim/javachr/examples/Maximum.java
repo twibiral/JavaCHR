371190e95d9879baa26ebec2e5781983abd8e6bf
timwibiral <tim.wibiral@uni-ulm.de>
2020-09-17 23:23:27 +0200

    Add parallel computing to Maximum

--- src/main/java/wibiral/tim/javachr/examples/Maximum.java
+++ src/main/java/wibiral/tim/javachr/examples/Maximum.java
@@ -11,26 +11,31 @@ import java.util.Random;
 public class Maximum {
     public static void main(String[] args) {
         Random ran = new Random();
-        Integer[] array = new Integer[10];
+        Integer[] array = new Integer[75000];
         for (int i = 0; i < array.length; i++) {
             array[i] = ran.nextInt(1000);
         }
 
-
-        // Sequential approach:
         Simpagation rule = new Simpagation(1, 1)
                 .guard((h1, h2) -> (Integer) h1[0].value() >= (Integer) h2[0].value());
 
+        ConstraintStore result;
+        long start, end;
+
+        // Sequential approach:
         ConstraintHandler handler = new SimpleHandler(rule);
-        ConstraintStore result = handler.solve(array);
-        System.out.println("Sequential computed: " + result.toString() + "\n");
+        start = System.currentTimeMillis();
+        result = handler.solve(array);
+        end = System.currentTimeMillis();
+        System.out.println("Sequential computed: " + result.toString() + "\nTime: " + (end - start) + "ms\n");
 
 
         // Semi-parallel approach:
         SemiParallelHandler parallelHandler = new SemiParallelHandler(8, rule);
-        ConstraintStore result2 = parallelHandler.solve(array);
-//        result2 = parallelHandler.solve(array);
-        System.out.println("Parallel computed: " + result2.toString());
+        start = System.currentTimeMillis();
+        result = parallelHandler.solve(array);
+        end = System.currentTimeMillis();
+        System.out.println("Parallel computed: " + result.toString() + "\nTime: " + (end - start) + "ms\n");
         parallelHandler.kill();
     }
 }
