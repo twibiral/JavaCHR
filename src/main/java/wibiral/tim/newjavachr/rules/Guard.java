ef8db6557dce5171303eb3be8db7dd939f1a97c1
timwibiral <tim.wibiral.tw@gmail.com>
2021-10-08 10:17:23 +0200

    Add copy of Guard.java

---
+++ src/main/java/wibiral/tim/newjavachr/rules/Guard.java
@@ -0,0 +1,18 @@
+package wibiral.tim.newjavachr.rules;
+
+import wibiral.tim.javachr.constraints.Constraint;
+
+/**
+ * Represents the lambda expression of the guard.
+ */
+public interface Guard {
+    /**
+     * Checks if defined conditions are true for the constraints in the array. Example:
+     * {@code head -> head[0] > head[1]}
+     * or
+     * {@code head -> (int) head[0].value() == 42}
+     * @param head An array with constraints which represent the constraints from the head.
+     * @return True if the constraints fulfill the conditions.
+     */
+    boolean check(Constraint<?>[] head);
+}
