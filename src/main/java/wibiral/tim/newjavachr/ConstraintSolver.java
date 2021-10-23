78d4d6fc4862f5a56087915a87bf66984e4e8a5a
timwibiral <tim.wibiral.tw@gmail.com>
2021-10-23 22:43:51 +0200

    First draft of ConstraintSolver interface

--- src/main/java/wibiral/tim/newjavachr/ConstraintSolver.java
+++ src/main/java/wibiral/tim/newjavachr/ConstraintSolver.java
@@ -1,4 +1,31 @@
 package wibiral.tim.newjavachr;
 
-public class ConstraintSolver {
+import java.util.List;
+
+/**
+ * Constraint solvers apply a set of rules to some given constraints.
+ * Classes that implement this interface provide different "solve" methods which return a list with constraints after the rules were applied.
+ */
+public interface ConstraintSolver {
+    /**
+     * Applies the defined rules to the given constraints.
+     * @param constraints the constraints you want the rules applied to.
+     * @return The result after no more rules can be applied to the constraints.
+     */
+    List<Constraint<?>> solve(Constraint<?>... constraints);
+
+    /**
+     * Applies the defined rules to the given constraints.
+     * @param constraints the constraints you want the rules applied to.
+     * @return The result after no more rules can be applied to the constraints.
+     */
+    List<Constraint<?>> solve(List<?>... constraints);
+
+    /**
+     * Applies the defined rules to the given objects.
+     * @param values Some objects you want the rules be applied to.
+     * @return The result after no more rules can be applied to the constraints.
+     */
+    <T> List<Constraint<?>> solve(T... values);
+
 }
