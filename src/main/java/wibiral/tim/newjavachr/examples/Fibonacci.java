11be792136feca55a0e37b8c1a068df6fb96c8cb
Tim Wibiral <tim.wibiral@gmail.com>
2021-10-28 13:07:46 +0200

    Add fibonacci example (Doesnt work because propagation doesnt work

---
+++ src/main/java/wibiral/tim/newjavachr/examples/Fibonacci.java
@@ -0,0 +1,71 @@
+package wibiral.tim.newjavachr.examples;
+
+import wibiral.tim.newjavachr.Constraint;
+import wibiral.tim.newjavachr.ConstraintSolver;
+import wibiral.tim.newjavachr.SimpleConstraintSolver;
+import wibiral.tim.newjavachr.rules.Propagation;
+import wibiral.tim.newjavachr.rules.Rule;
+import wibiral.tim.newjavachr.rules.Simpagation;
+import wibiral.tim.newjavachr.tracing.CommandLineTracer;
+
+import java.util.List;
+
+public class Fibonacci {
+    public static class fib {
+        final int a;
+        final long b;
+
+        fib(int a, long b){
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public String toString(){
+            return "fib(" + a + ", " + b + ")";
+        }
+
+        @Override
+        public boolean equals(Object obj){
+            return obj instanceof fib && ((fib) obj).a == this.a && ((fib) obj).b == this.b;
+        }
+    }
+
+    public static void main(String[] args) {
+        ConstraintSolver fibHandler = new SimpleConstraintSolver(getRules());
+        fibHandler.setTracer(new CommandLineTracer(true));
+
+        System.out.println("Fibonacci 42:");
+        long start = System.currentTimeMillis();
+        List<Constraint<?>> result = fibHandler.solve(42);
+        long end = System.currentTimeMillis();
+        System.out.println(result + "\nDuration: " + (end - start) + "ms");
+    }
+
+    static Rule[] getRules(){
+        Rule r1 = new Propagation(1)  // MAX => fib(0, 1), fib(1, 1).
+                .guard(x -> x[0].value() instanceof Integer)
+                .body((oldC, newC) -> {
+                    newC.add(new Constraint<>(new fib(0, 0)));
+                    newC.add(new Constraint<>(new fib(1, 1)));
+                });
+
+        Rule r2 = new Propagation(3)
+                // MAX, fib(N1, M1), fib(N2, M2) =>  N1 == N2 - 1 | fib(N2 + 1, M1 + M2).
+                .guard(x -> x[0].value() instanceof Integer && x[1].value() instanceof fib && x[2].value() instanceof fib
+                        && ((fib) x[1].value()).a == ((fib) x[2].value()).a - 1
+                        && ((fib) x[2].value()).a < (int) x[0].value()
+                ).body((oldC, newC) -> {
+                    int N2 = ((fib) oldC[2].value()).a;
+                    long M1 = ((fib) oldC[1].value()).b;
+                    long M2 = ((fib) oldC[2].value()).b;
+                    newC.add(new Constraint<>(new fib(N2+1, M1 + M2)));
+                });
+
+        Rule r3 = new Simpagation(1, 1)
+                .guard((h1, h2) -> h1[0].value() instanceof fib && h2[0].value() instanceof Integer
+                        && h2[0].equals(((fib) h1[0].value()).a));
+
+        return new Rule[]{r1, r2, r3};
+    }
+}
