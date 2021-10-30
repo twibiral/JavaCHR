c2c75222b4c92f7d552fc3728af6b52fc8c4d8a4
timwibiral <tim.wibiral.tw@gmail.com>
2021-10-30 16:24:49 +0200

    Implemented PropagationHistory

--- src/main/java/wibiral/tim/newjavachr/constraints/PropagationHistory.java
+++ src/main/java/wibiral/tim/newjavachr/constraints/PropagationHistory.java
@@ -2,13 +2,48 @@ package wibiral.tim.newjavachr.constraints;
 
 import wibiral.tim.newjavachr.rules.Rule;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * The propagation is used to store which constraints were applied to which constraints. This is used to prevent
+ * propagation rules from firing multiple times for the same constraints.
+ * (Propagation doesn't remove any constraints from the constraint store and could therefore fire possibly fire multiple
+ * time for the same constraints if no history is saved.)
+ */
 public class PropagationHistory {
-    private static class HistoryElement {
+    // A history list for every rule.
+    HashMap<Long, ArrayList<HistoryEntry>> history = new HashMap<>();
+
+    /**
+     * Adds a new History Entry to the propagation history with the rule and the constraints that were applied to the rule.
+     */
+    public void addEntry(Rule rule, Constraint<?>[] constraints){
+        if(!history.containsKey(rule.ID())){
+            history.put(rule.ID(), new ArrayList<>());
+        }
+
+        history.get(rule.ID()).add(new HistoryEntry(rule, constraints));
+    }
+
+    /**
+     * This class is used to store a single entry in the Propagation History and contains the ID of a rule and the
+     * IDs of the constraints that were applied to the rule.
+     */
+    private static class HistoryEntry {
         final long ruleID;
         final long[] constraintIDs;
+        final int nrConstraints;
 
-        HistoryElement(Rule rule, Constraint<?>[] constraints){
+        HistoryEntry(Rule rule, Constraint<?>[] constraints){
             ruleID = rule.ID();
+            nrConstraints = constraints.length;
+            constraintIDs = new long[nrConstraints];
+
+            for (int i = 0; i < nrConstraints; i++) {
+                constraintIDs[i] = constraints[i].ID();
+            }
         }
     }
 }
