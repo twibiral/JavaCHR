3296cd57099b2aade13a7fa4207a078cc0d86a78
timwibiral <tim.wibiral.tw@gmail.com>
2021-11-15 17:41:48 +0100

    Single class exports

--- src/main/java/wibiral/tim/newjavachr/constraints/ConstraintStore.java
+++ src/main/java/wibiral/tim/newjavachr/constraints/ConstraintStore.java
@@ -1,6 +1,11 @@
 package wibiral.tim.newjavachr.constraints;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 
 
 /**
