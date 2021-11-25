d25242320f6dc923bd8eb3fd7642a675b8266329
Tim Wibiral <tim.wibiral@gmail.com>
2021-11-25 14:38:38 +0100

    Implement validation

--- src/main/java/wibiral/tim/javachr/examples/webserver/HTTP_Request.java
+++ src/main/java/wibiral/tim/javachr/examples/webserver/HTTP_Request.java
@@ -1,22 +1,26 @@
 package wibiral.tim.javachr.examples.webserver;
 
+import java.io.File;
 import java.net.Socket;
+import java.util.Locale;
 
 public class HTTP_Request {
-    private String request;
+    private static final String RESOURCE_PREFIX = "http/";
+
     private Socket sender;
+    private String method, resource, protocol;
 
 
 
-    public HTTP_Request(String request, Socket sender){
-        this.request = request;
+    public HTTP_Request(Socket sender, String method, String resource, String protocol){
         this.sender = sender;
+        this.method = method.trim().toLowerCase(Locale.ROOT);
+        this.protocol = protocol;
 
-
-    }
-
-    public String getRequest(){
-        return request;
+        this.resource = resource.trim().equals("/") ? "index.html" : resource.trim().toLowerCase(Locale.ROOT); // map "/" to "index.html"
+        if(this.resource.startsWith("/"))
+            this.resource = resource.substring(1); // Remove first "/"
+        this.resource = RESOURCE_PREFIX + resource;
     }
 
     public Socket getSender(){
@@ -24,7 +28,12 @@ public class HTTP_Request {
     }
 
     public boolean isValid() {
-        return false;
+        boolean methodAccepted = method.equals("get") || method.equals("head");
+
+        File f = new File(resource);
+        boolean fileExists = f.exists() && !f.isDirectory();
+
+        return methodAccepted && fileExists;
     }
 
     public HTTP_Response getResponse() {
