16e32eaeb3ebfd214252da5a99ddd27bb3a078dd
Tim Wibiral <tim.wibiral@gmail.com>
2021-12-16 14:36:35 +0100

    Add extension

---
+++ src/main/java/wibiral/tim/javachr/extensions/RulePrioritizingApplicator.java
@@ -0,0 +1,19 @@
+package wibiral.tim.javachr.extensions;
+
+import wibiral.tim.javachr.SimpleRuleApplicator;
+
+/**
+ * This class implements CHR^RP semantics as a rule applicator.
+ * That means that it will apply rules in the order of their priority. The priority
+ * of a rule is defined by the order of the parameters given to the constructor.
+ *
+ * In: {@code RuleApplicator ra = new RulePrioritizingApplicator(r1, r2, r3);}
+ * the rules will be applied in the order of r1, r2, r3. The first rule that is given to
+ * the constructor will be the rule with the highest priority.
+ *
+ * Note: This class simple delegates to {@link SimpleRuleApplicator} because
+ * {@link SimpleRuleApplicator} already implements the required semantics implicitly.
+ */
+public class RulePrioritizingApplicator extends SimpleRuleApplicator {
+    // CHR^RP semantics already implemented implicitly in SimpleRuleApplicator.
+}
