84d0dead24b18777cad81661c70afb96214a8665
Tim Wibiral <tim.wibiral.tw@gmail.com>
2021-12-17 20:33:16 +0100

    Collapse into one class

--- src/main/java/javachr/extensions/higherorderrules/Twice.java
+++ src/main/java/javachr/extensions/higherorderrules/Twice.java
@@ -2,29 +2,61 @@ package javachr.extensions.higherorderrules;
 
 import javachr.constraints.Constraint;
 import javachr.rules.Rule;
+import javachr.rules.head.HEAD_DEFINITION_TYPE;
+import javachr.rules.head.Head;
+import javachr.rules.head.VAR;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Twice extends HigherOrderBase {
+public class Twice extends Rule {
+    private final Rule rule;
+
     protected Twice(Rule rule) {
-        super(rule, "Twice");
+        super("Twice " + rule.name(), rule.headSize());
+        this.rule = rule;
     }
 
     @Override
     public boolean accepts(List<Constraint<?>> constraints) {
-        return this.rule.accepts(constraints);
+        return rule.accepts(constraints);
     }
 
     @Override
     public List<Constraint<?>> apply(List<Constraint<?>> constraints) {
-        List<Constraint<?>> result1 = this.rule.apply(new ArrayList<>(constraints));// Works on copy
-        List<Constraint<?>> result2 = this.rule.apply(constraints);
+        List<Constraint<?>> result1 = rule.apply(new ArrayList<>(constraints));// Works on copy
+        List<Constraint<?>> result2 = rule.apply(constraints);
 
         return Stream.concat(result1.stream(), result2.stream())    // join lists
                 .distinct()                                         // remove duplicates
                 .collect(Collectors.toList());                      // convert to list
+    }
+
+    @Override
+    public HEAD_DEFINITION_TYPE getHeadDefinitionType(){
+        return rule.getHeadDefinitionType();
+    }
+
+    @Override
+    public Class<?>[] getHeadTypes(){
+        return rule.getHeadTypes();
+    }
+
+    @Override
+    public Head[] getHeadDefinitions(){
+        return rule.getHeadDefinitions();
+    }
+
+    @Override
+    public EnumMap<VAR, ArrayList<Integer>> getVariableBindings(){
+        return rule.getVariableBindings();
+    }
+
+    @Override
+    public boolean saveHistory() {
+        return rule.saveHistory();
     }
 }
