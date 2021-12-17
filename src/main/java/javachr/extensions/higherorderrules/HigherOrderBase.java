b5d1507e98f914444c36591688672997e4a9eef8
Tim Wibiral <tim.wibiral.tw@gmail.com>
2021-12-17 18:53:21 +0100

    Add super class for higher order functions

---
+++ src/main/java/javachr/extensions/higherorderrules/HigherOrderBase.java
@@ -0,0 +1,46 @@
+package javachr.extensions.higherorderrules;
+
+import javachr.rules.Rule;
+import javachr.rules.head.HEAD_DEFINITION_TYPE;
+import javachr.rules.head.Head;
+import javachr.rules.head.VAR;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+
+/**
+ * This class is the base class for all higher order rules.
+ * Takes a rule and stores it. Subclasses can then use this rule.
+ * It overrides some methods of the Rule class to pass through the method calls to the rule that is wrapped.
+ *
+ * Example: {@link Twice} takes the stored rule and applies it twice to the given constraints.
+ */
+public abstract class HigherOrderBase extends Rule {
+    protected final Rule rule;
+
+    protected HigherOrderBase(Rule rule, String prefix) {
+        super(prefix + rule.name(), rule.headSize());
+        this.rule = rule;
+    }
+
+    @Override
+    public HEAD_DEFINITION_TYPE getHeadDefinitionType(){
+        return rule.getHeadDefinitionType();
+    }
+
+    @Override
+    public Class<?>[] getHeadTypes(){
+        return rule.getHeadTypes();
+    }
+
+    @Override
+    public Head[] getHeadDefinitions(){
+        return rule.getHeadDefinitions();
+    }
+
+    @Override
+    public EnumMap<VAR, ArrayList<Integer>> getVariableBindings(){
+        return rule.getVariableBindings();
+    }
+
+}
