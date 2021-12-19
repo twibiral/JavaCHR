7a1a8a635687b184808aa753cb3be4eb0de549d2
Tim Wibiral <tim.wibiral@gmail.com>
2021-12-19 14:49:11 +0100

    Improved

--- src/main/java/javachr/extensions/ConcurrentStore.java
+++ src/main/java/javachr/extensions/ConcurrentStore.java
@@ -7,62 +7,15 @@ import java.util.*;
 
 /**
  * Concurrency safe version of {@link ConstraintStore}.
- * Synchronizes all methods that add or remove constraints. Doesn't synchronize get methods.
+ * Synchronizes all methods that remove constraints. Doesn't synchronize get or add methods.
  */
 public class ConcurrentStore extends ConstraintStore {
     private final List<Constraint<?>> store = new ArrayList<>();
 
     /**
-     * Add the given constraint to the store.
-     */
-    public void add(Constraint<?> constraint){
-        store.add(constraint);
-        // All rules in the store must be alive if they aren't in use:
-        constraint.setAlive();
-    }
-
-    /**
-     * Create a new constraint and add it to the store.
-     * @param object the new object to add.
-     * @param <T> Type of the given object and the created constraint.
-     */
-    public synchronized <T> void add(T object){
-        this.add(new Constraint<>(object));
-    }
-
-    /**
-     * Adds all the constraints of the list to the constraint store.
-     * @param collection Constraints that get added to the store.
-     */
-    public void addAll(Collection<Constraint<?>> collection){
-        HashSet<Constraint<?>> withoutDuplicates = new HashSet<>(collection);
-
-        synchronized (this) {
-            store.addAll(withoutDuplicates);
-        }
-
-        // All rules in the store must be alive if they aren't in use:
-        withoutDuplicates.forEach(Constraint::setAlive);
-    }
-
-    /**
-     * Adds all the constraints of the list to the constraint store.
-     * @param constraints Constraints that get added to the store.
-     */
-    public void addAll(Constraint<?>... constraints){
-        HashSet<Constraint<?>> withoutDuplicates = new HashSet<>(Arrays.asList(constraints));
-
-        synchronized (this) {
-            store.addAll(withoutDuplicates);
-        }
-
-        // All rules in the store must be alive if they aren't in use:
-        withoutDuplicates.forEach(Constraint::setAlive);
-    }
-
-    /**
      * Remove the constraint with the given ID.
      */
+    @Override
     public synchronized void remove(long ID){
         store.forEach(x -> {
             if(x.getID() == ID){
@@ -75,6 +28,7 @@ public class ConcurrentStore extends ConstraintStore {
     /**
      * Set all constraints to dead and remove them from the internal data structure.
      */
+    @Override
     public synchronized void clear(){
         store.forEach(Constraint::setDead);
         store.clear();
